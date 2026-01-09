858170d15870a4de2b18a9a56b291ad7e1c31af1
JOCHEN <jochen.joerg@snowflake.com>
2026-01-09 19:55:07 +0000

    Changed references to github repo paths to match github repo named github_repo_snowflake_intelligence_demos_jojrg and branch energy_company_enhancements.

---
+++ setup_via_snowflake_workspace/setup_initial_ws.sql
@@ -0,0 +1,34 @@
+USE ROLE ACCOUNTADMIN;
+
+-- Create a warehouse
+CREATE WAREHOUSE IF NOT EXISTS AI_WH WITH WAREHOUSE_SIZE='X-SMALL';
+
+-- Create a fresh Database
+CREATE OR REPLACE DATABASE AI_DEVELOPMENT;
+
+-- Create the API integration with Github
+
+/**
+CREATE OR REPLACE API INTEGRATION GITHUB_INTEGRATION_SNOWFLAKE_INTELLIGENCE_DEMOS
+    api_provider = git_https_api
+    api_allowed_prefixes = ('https://github.com/michaelgorkow/')
+    enabled = true
+    comment='Git integration for Github Repository from Michael Gorkow.';
+
+
+-- Create the integration with the Github demo repository
+CREATE GIT REPOSITORY GITHUB_REPO_SNOWFLAKE_INTELLIGENCE_DEMOS
+	ORIGIN = 'https://github.com/michaelgorkow/snowflake-intelligence-demos' 
+	API_INTEGRATION = 'GITHUB_INTEGRATION_SNOWFLAKE_INTELLIGENCE_DEMOS' 
+	COMMENT = 'Github Repository from Michael Gorkow with demos for Cortex Agents.';
+**/
+
+-- Create the integration with the Github demo repository
+CREATE GIT REPOSITORY GITHUB_REPO_SNOWFLAKE_INTELLIGENCE_DEMOS_JOJRG
+	ORIGIN = 'https://github.com/jojrg/snowflake-intelligence-demos' 
+	API_INTEGRATION = 'GITHUB_OAUTH_JOJRG' 
+	COMMENT = 'Github Repository from Jochen Joerg with demos for Cortex Agents.';
+
+
+-- Run the installation of the Demo
+EXECUTE IMMEDIATE FROM @AI_DEVELOPMENT.PUBLIC.GITHUB_REPO_SNOWFLAKE_INTELLIGENCE_DEMOS_JOJRG/branches/energy_company_enhancements/setup/setup.sql;
